299476d5024bfc72d64ac7c616e3d6e3be54b3f2
jelambrar96 <jelambrar@gmail.com>
2024-09-21 14:27:55 -0500

    use python 3.9 and guinicorn

--- project_003_bloggin_platform/run.sh
+++ project_003_bloggin_platform/run.sh
@@ -8,5 +8,5 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python manage.py createsuperuser --noinput
 
-# gunicorn --bind 0.0.0.0:8000 todo_webapp.wsgi:application
-python3 manage.py runserver "0.0.0.0:8000"
+gunicorn --bind 0.0.0.0:8000 blogging_platform.wsgi:application
+# python3 manage.py runserver "0.0.0.0:8000"
