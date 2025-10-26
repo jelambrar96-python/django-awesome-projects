380fd593ab7180a24ef9c404b2782be9635758ff
jelambrar96 <jelambrar@gmail.com>
2025-10-26 15:32:34 -0500

    project_018_phone_number_authentication

---
+++ project_018_phone_number_authentication/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
+
+class GFG(AbstractUser):
+    phone = models.CharField(max_length=12, unique=True)
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
