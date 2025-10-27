dc4e33c780c3233942107051d817b8966e914c2d
jelambrar96 <jelambrar@gmail.com>
2025-10-26 22:22:28 -0500

    docker

--- project_001_todo_webapp/docker-run.sh
+++ project_001_todo_webapp/docker-run.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-docker build -t django-todo-app .  && \
-    docker run -p 8000:8000 django-todo-app
+docker build -t dap001-todo-app .  && \
+    docker run -p 8000:8000 dap001-todo-app
