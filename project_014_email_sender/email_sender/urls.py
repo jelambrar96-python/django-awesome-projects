87f2524a16fbb2f1bb0d772c31bb822960795dbc
jelambrar96 <jelambrar@gmail.com>
2025-10-30 15:24:11 -0500

    email sender

--- project_014_email_sender/email_sender/urls.py
+++ project_014_email_sender/email_sender/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from home import views as home_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_views.send_mail_page),
