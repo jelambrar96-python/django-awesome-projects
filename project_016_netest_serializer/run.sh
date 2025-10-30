2f68bee21eab253611eba1ec14cdd60093e8c4a1
jelambrar96 <jelambrar@gmail.com>
2025-10-30 15:54:42 -0500

    jelambrar96/project_016_netest_serializer

--- project_016_netest_serializer/run.sh
+++ project_016_netest_serializer/run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# echo username $DJANGO_SUPERUSER_USERNAME
-# echo password $DJANGO_SUPERUSER_PASSWORD
-# echo email $DJANGO_SUPERUSER_EMAIL
+python3 manage.py makemigrations
+python3 manage.py migrate --noinput
 
-# python3 manage.py makemigrations
-# python3 manage.py migrate
-
-
-python manage.py createsuperuser --noinput
-gunicorn --bind 0.0.0.0:8000 core.wsgi:application
+exec "$@"
